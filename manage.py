0f6eb3218de4bbccb4731a0cdc521b0f8ca46af5
Sumit Lohan <sumit.lohan@icanbwell.com>
2024-06-16 13:16:01 +0530

    Added db healthcheck and starting web service only if db service is ready to use

--- manage.py
+++ manage.py
@@ -11,28 +11,6 @@ from django.db import connections
 from social_networking.settings import Settings
 
 
-def wait_for_db_connection():
-    # Maximum number of retries
-    max_retries = 10
-    for alias in connections:
-        for attempt in range(max_retries):
-            connection = connections[alias]
-            try:
-                # Attempt to establish a database connection
-                connection.ensure_connection()
-                print(f"Connected to database '{alias}'!", flush=True)
-                return
-            except OperationalError as e:
-                print(
-                    f"Attempt {attempt + 1}/{max_retries}: "
-                    f"Unable to connect to database '{alias}'. Retrying in 2 seconds...", flush=True
-                )
-                time.sleep(2)
-    # If maximum retries reached without success
-    raise OperationalError("Unable to establish a connection to the database.")
-
-
-
 def main():
     """Run administrative tasks."""
     try:
@@ -50,6 +28,4 @@ if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_networking.settings')
     # configure class based settings.
     settings.configure(Settings())
-    # It is possible that our other services initialise prior to our database connection. So, wait until our database connection setUp.
-    wait_for_db_connection()
     main()
